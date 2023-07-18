6f0caa4498a442be6133bb7e4ff6b867a303b5b0
Jules <jules.hohmann@gmail.com>
2023-07-18 16:38:41 -0400

    began c++ problems

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector> 
+#include <cmath>
 int add(int x, int y)
 {
   return x + y;
@@ -17,6 +18,67 @@ int division(int x, int y)
   return x/y;
 }
 
+class circle
+  {
+    public:
+      int Radius;
+      int area()
+      {
+        return M_1_PI*Radius*Radius;
+      }
+  };
+class point
+{
+  public:
+  double x;
+  double y;
+
+  int distance_to_origin()
+  {
+    return hypot(x, y);
+  }
+  
+  int distance_to_point(point a){
+    int dx = a.x -this->x;
+    int dy = a.y - this->y;
+    return hypot(dx, dy);
+  }
+
+};
+
+class line{
+  point p1;
+  point p2;
+  int length(){
+    int dx = this->p1.x-this->p2.x;
+    int dy = this->p2.x-this->p2.x;
+    return hypot(dx, dy);
+  }
+  int distance_to_point(point a){
+    int slope = (this->p1.y- this->p2.y )/(this->p1.x-this->p2.x);
+    int b = p1.y-slope*p1.x;
+    int d = abs(slope*a.x-slope*a.y+b)/hypot(slope, b);
+    return d;
+  }
+};
+
+class Triangle{
+  point p1;
+  point p2;
+  point p3;
+  int area(){
+    int A = 0.5 * abs(p1.x*(p2.y-p3.y)+p2.x*(p3.y-p1.y)+p3.x*(p1.y-p2.y));
+  }
+
+};
+class Polygon{
+  std:: vector<point> vertices;
+  int area (){
+    for int i in 
+
+  }
+
+};
 int main()
 {
   std::cout << "Hello, world!" << std::endl;
@@ -30,5 +92,11 @@ int main()
   std::cout <<"The quotiont of" <<x<< "and" <<y<< "is" << x/y<< "also"<<v<< "is equal to "<<v<< std::endl;
   std::cout <<"Jules Hohmann, AUV" << std::endl;
 
+  
+  circle c;
+  c.Radius = 5;
+  int area = c.area();
+  std::cout <<"The area of circle is"<<area<< std::endl;
+  
   return 0;
 }
